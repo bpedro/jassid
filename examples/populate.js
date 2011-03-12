2aca5610ad322f3ea2dc38a829660a0e093ed60e
Bruno Pedro <bpedro@tarpipe.com>
2011-03-12 22:32:38 +0000

    Populate is now non-recursive.

--- examples/populate.js
+++ examples/populate.js
@@ -3,20 +3,13 @@ var map = require('../lib/map');
 
 var names = ['John Doe', 'Lisa Simpson', 'Mark Silva', 'Rod Stewart', 'Michael Jackson'];
 
-function populate(total) {
-  if (total <= 0) {
-    return;
-  }
-  total = total || 1000;
-
+for (var total=1; total <= 1000; total++) {
   var data = {'name': names[Math.floor(Math.random() * names.length)], 'age': Math.floor(Math.random() * 61) + 20, 'index': total};
   store.save('/tmp/store', data, undefined, function(err, data, jsonData) {
     if (!err) {
       console.log(data.index + ' ' + data._meta.id);
-      populate(total - 1)
     } else {
       console.log(err);
     }
   });
 }
-populate(1000);
